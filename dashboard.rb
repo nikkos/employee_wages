9d21b5ffd8ed238bc47fd604e531e40ed09f5ee2
nikkos <nlampr@gmail.com>
2014-03-04 23:51:15 +0200

    First Commit

---
+++ dashboard.rb
@@ -0,0 +1,68 @@
+require_relative 'worker'
+class Dashboard
+  attr_accessor :title, :workers
+  
+  def initialize(title)
+    @title = title  
+    @workers= []
+  end
+  
+  def add_worker(a_worker)
+    @workers.push(a_worker)
+  end
+  
+  def print_stats
+    @workers.each do |w|
+      puts w.to_s
+    end
+    
+    
+    @workers.each do |w|
+      puts w.name
+      puts w.overtime_hours
+    end
+  
+    puts "Total Wages:"
+    puts @workers.reduce(0) { |sum, w| sum + w.pay }
+     
+    puts "Total Overtime Hours:"
+    puts @workers.reduce(0) { |sum, w| sum + w.overtime_hours}
+    
+    puts "Total Normal Hours"
+  end
+  
+end
+
+
+worker1 = Worker.new("nikkos", 10, 5.5 )
+worker2 = Worker.new("Giannis", 40, 10)
+worker3 = Worker.new("Kostas", 40, 8)
+worker4 = Worker.new("Ntinos", 25, 10)
+worker5 = Worker.new("Fanis", 60, 10)
+worker6 = Worker.new("Manos", 0, 10)
+
+worker1.countrate
+worker1.wages
+
+worker2.countrate
+worker2.wages
+
+worker3.countrate
+worker3.wages
+
+worker4.countrate
+worker4.wages
+
+worker5.countrate
+worker5.wages
+
+worker6.countrate
+worker6.wages
+
+demo1 = Dashboard.new("Demo1")
+demo1.add_worker(worker1)
+demo1.add_worker(worker2)
+demo1.add_worker(worker4)
+demo1.add_worker(worker5)
+demo1.add_worker(worker6)
+demo1.print_stats
---
+++ worker.rb
@@ -0,0 +1,86 @@
+class Worker
+  
+  attr_accessor :name, :hours, :payrate, :pay, :overtime, :regular
+  
+  
+  def initialize(name, hours, payrate=0.0)
+    @name = name
+    @hours = hours
+    @payrate = payrate
+    @pay = 0 
+  end
+  
+  def countrate
+    if @hours <= 35
+      @regular = @hours * @payrate
+      @overtime = 0.0
+    else
+      self.overtime_calc
+    end
+  end
+  
+  def overtime_calc
+    @regular = 35 * @payrate
+    @overtime = (@hours - 35) * @payrate * 1.5 
+  end
+  
+  def wages
+    @pay = @regular + @overtime
+    if @pay  <= 20
+      @pay = 20
+    else
+      puts "hello"
+    end
+  end
+  
+  def overtime_hours
+    if @hours >= 35
+      @overtime = @hours - 35
+    else
+      @hours = 0
+    end
+  end
+  
+  def normal_hours
+    
+  end
+  
+  def to_s
+    "#{@name} work #{@hours} and his rate is #{@payrate} should be paid: #{@pay} "
+  end
+end
+
+if __FILE__ == $0
+worker1 = Worker.new("nikkos", 10, 5.5 )
+worker2 = Worker.new("Giannis", 40, 10)
+worker3 = Worker.new("Kostas", 40, 8)
+worker4 = Worker.new("Ntinos", 25, 10)
+worker5 = Worker.new("Fanis", 60, 10)
+worker6 = Worker.new("Manos", 0, 10)
+
+worker1.countrate
+worker1.wages
+
+worker2.countrate
+worker2.wages
+
+worker3.countrate
+worker3.wages
+
+worker4.countrate
+worker4.wages
+
+worker5.countrate
+worker5.wages
+
+worker6.countrate
+worker6.wages
+
+
+puts worker1
+puts worker2
+puts worker3
+puts worker4
+puts worker5
+puts worker6
+end
